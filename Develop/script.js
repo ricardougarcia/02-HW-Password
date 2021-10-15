16aca90649178e1fe23c62eab1663a49b4856def
Ricardo Garcia <ricardougarcia@gmail.com>
2021-10-14 22:14:43 -0400

    added for loop for passcode.

--- Develop/script.js
+++ Develop/script.js
@@ -18,33 +18,53 @@ function generatePassword() {
   var passwordlength = parseFloat(totalcharacters);
 
   //validating entry is within parameters. If not then alert!
+  //First line validates length with boolean
   var minpasswordlength = passwordlength > 7;
   var maxpasswordlength = passwordlength < 128;
   if (minpasswordlength && maxpasswordlength) {
-    console.log("True ✅ True ✅");
+    console.log(passwordlength);
+
+    // User selects uppercase - boolean
     var uppercase = confirm("Do you want uppercase characters?");
     // console.log(uppercase);
     var lowercase = confirm("Do you want lowercase letters?");
+    // User selects numbers- boolean
     var numbers = confirm("Do you want numbers?");
+    // User selects characters- boolean
     var special = confirm("Do you want special characters?");
+
+    // This is my array of password characters.
+    var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+    var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "0123456789";
+    var symbols = "!@#$%^&*_-+=";
+
+    // This is my candidate pool of selected characters
+    // var candidatePool = alphaLower + alphaUpper + numbers + symbols;
+    // console.log(candidatePool);
+
+    // This is my function for looping selected characters
+    // var passwordCandidate = "";
+    // for(int i = 0; i < passwordLength; i++) {
+    //   //var randChar = // get your randomly access character from the candidate pool
+    //   passwordCandidate = passwordCandidate + randChar;
+
+    var randomAlphaLower = "";
+    console.log(randomAlphaLower);
+    // This is my loop equivalent to the selected password length
+    for (var i = 0; i < passwordlength; i++) {
+      var generatedLower =
+        alphaLower[Math.floor(Math.random() * alphaLower.length)];
+      console.log(randomAlphaLower);
+      randomAlphaLower = randomAlphaLower + generatedLower;
+      console.log(randomAlphaLower);
+    }
+    console.log("result: " + randomAlphaLower);
+    return randomAlphaLower;
   } else {
     console.log("False ❌ False ❌");
     alert("Please select a number between 8 - 128");
   }
-
-  // const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  // const numbers = "0123456789";
-  // const symbols = "!@#$%^&*_-+=";
-
-  // Write Your JavaScript Code Here
-
-  //do stuff
-  //get the length the user wants
-  //if it's not at least 8 characters no more than 128... start over
-  // do you want uppercase
-  //do you want lowercase
-  // do you want numbers
-  //do you want special characters
 }
 
 // Add event listener to generate button
